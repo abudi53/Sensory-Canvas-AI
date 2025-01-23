947251d42d96b670da099b37c575ff62eed7d2a5
abudi53 <abdallahffh@gmail.com>
2025-01-23 15:43:39 -0400

    add gen-image endpoint

---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import generate_image, generate_audio
+
+urlpatterns = [
+    path('generate_image/', generate_image, name='generate_image'),
+    path('generate_audio/', generate_audio, name='generate_audio'),
+]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from huggingface_hub import InferenceClient
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
+from io import BytesIO
+import base64
 
-# Create your views here.
+
+
+@api_view(['POST'])
+def generate_image(request):
+    client = InferenceClient("black-forest-labs/FLUX.1-dev", token=settings.HF_API_KEY)
+    image = client.text_to_image("Astronaut riding a horse")
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    
+    return Response({'image': img_str})
+
+@api_view(['POST'])
+def generate_audio(request):
+    client = InferenceClient("2Noise/ChatTTS", token=settings.HF_API_KEY)
+    audio = client.text_to_speech("Hello, how are you doing today?")
+    return Response({'audio': audio})
